f6a858678f2f338c9e1980f3fb99ec552f502ea8
Théo Alloucha <talloucha@innovqube.com>
2024-06-19 23:02:17 +0200

    Editions Tables \ Migrations

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(default=0,
+                                             validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+                                             )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
