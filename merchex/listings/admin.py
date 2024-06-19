db5f53ebf78c45dc1fd09c54a983e802c3860be2
Théo Alloucha <talloucha@innovqube.com>
2024-06-19 23:46:43 +0200

    Ajout listing , Relations OneToMany

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from listings.models import Band, Listing
+
 
 # Register your models here.
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')  # liste les champs que nous voulons sur l'affichage de la liste
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'sold', 'band_id')  # liste les champs que nous voulons sur l'affichage de la liste
+
+
+admin.site.register(Band, BandAdmin)  # nous modifions cette ligne, en ajoutant un deuxième argument
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -3,6 +3,15 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
+    # La Methode Magique "__str__"
+    # sert a renvoyé la chaine de caractère dans le panel admin
+    # et non "Band object (id number)"
+
+    # Nous avons modifié la façon dont un Band est représenté sous forme de chaîne de caractères
+    # en utilisant la méthode __str__
+    def __str__(self):
+        return f'{self.name}'
+
     class Genre(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
@@ -16,3 +25,22 @@ class Band(models.Model):
                                              )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
+
+
+class Listing(models.Model):
+    def __str__(self):
+        return f'{self.title}'
+    class ListingType(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHING = 'C'
+        POSTERS = 'P'
+        MISC = 'M'
+
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=100)
+    type = models.fields.CharField(choices=ListingType.choices, max_length=5)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(default=0,
+                                      validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+                                      )
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
